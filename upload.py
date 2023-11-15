2a0488b97495e8498565590f6a4ad952390ba7de
tdambrowitz <trenton.dambrowitz@haloarc.co.uk>
2023-11-15 13:39:22 +0000

    Update upload.py

--- upload.py
+++ upload.py
@@ -30,13 +30,31 @@ def display_page():
 
     st.title('Halo Dashboard - Upload BMS Reports')
     
-    with st.expander("How do I run the report?"):
+    with st.expander("How do I run the Due In report?"):
         st.write('1. Navigate to the "Job Listing" report (found under "Job Analysis" in BMS)')
         st.write('2. Set the "Date" selector to "Scheduled Onsite"')
         st.write('3. Set the "From Date" selector to the current date')
         st.write('4. Set the "To Date" selector as far out as you want to go (e.g. 4 weeks)')
         st.write('5. Click "Print" then close the excel file that opens (you can save it somewhere if you want)')
         st.write('6. Come back to this page and upload the file (it should start with "job_list" and end with ".csv")')
+
+    with st.expander("How do I run the On-Site report?"):
+        st.write('1. Navigate to the "Job Listing" report (found under "Job Analysis" in BMS)')
+        st.write('2. Set the "Date" selector to "Scheduled Onsite"')
+        st.write('3. Set the "From Date" selector to the current date')
+        st.write('4. Set the "To Date" selector as far out as you want to go (e.g. 4 weeks)')
+        st.write('5. Click "Print" then close the excel file that opens (you can save it somewhere if you want)')
+        st.write('6. Come back to this page and upload the file (it should start with "job_list" and end with ".csv")')
+
+    with st.expander("How do I run the Arrived Yesterday report?"):
+        st.write('1. Navigate to the "Job Listing" report (found under "Job Analysis" in BMS)')
+        st.write('2. Set the "Date" selector to "Scheduled Onsite"')
+        st.write('3. Set the "From Date" selector to the current date')
+        st.write('4. Set the "To Date" selector as far out as you want to go (e.g. 4 weeks)')
+        st.write('5. Click "Print" then close the excel file that opens (you can save it somewhere if you want)')
+        st.write('6. Come back to this page and upload the file (it should start with "job_list" and end with ".csv")')
+
+        
 
 
 
